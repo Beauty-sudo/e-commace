749660aab48f7ec7103c1b4f83ee9cd8572775b1
opeyemi <operaomilude111@gmail.com>
2022-12-25 23:40:24 +0100

    commace

---
+++ js/scrpt.js
@@ -0,0 +1,23 @@
+// alert("Working....")
+let url = 'https://dummyjson.com/products/'
+
+fetch(url)
+.then(res => res.json())
+.then(data =>{
+    console.log(data.products[0])
+    domManipulate(data, url)
+})
+function domManipulate(data){
+    const row = document.getElementById("row")
+    data.products.forEach(product => {
+        row.innerHTML += `
+        <div class="col-sm">
+        <a href="${url}/${product.id}">
+        <img src="${product.thumbnail}" class="img" alt">
+        </a>
+        <p>${product.title}</p>
+        <span># ${product.price}</span>
+        </div>
+        `
+    })
+}
